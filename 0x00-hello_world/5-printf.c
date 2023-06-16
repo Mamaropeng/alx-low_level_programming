38e42846e47f97812e97d26c3ee3c646515f1f16
Mamaropeng <tsholomr12@gmail.com>
2023-06-16 13:17:12 -0700

    print and compile a text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print a text and compile
+ * main - paint and compile a text
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (sucess)
  *
  */
 int main(void)
 {
-	printf(gcc - \"with proper grammar, but the outcome is a piece of art, \n”);
+	printf("with proper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
 
