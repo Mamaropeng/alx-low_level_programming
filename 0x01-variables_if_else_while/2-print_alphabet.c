d83d69b3d4bee5fca1f35d9edd28c4c696730db4
Mamaropeng <tsholomr12@gmail.com>
2023-06-19 15:46:44 -0700

    prints the alphabet in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,18 +3,18 @@
 /**
  * main- Entry point
  * Description: print alphabet in lowercase
- * Return: Always 0
- *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n = 97;
 
 		while
-			(n <= 122);
+		(n <= 122);
 	{
 		putchar(n);
 		n++;
+		return (0);
 	}
 	putchar('\n');
 	return (0);
