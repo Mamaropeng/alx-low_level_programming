7b7ef9975920064c1c7a66672a119d07afa2b899
Mamaropeng <tsholomr12@gmail.com>
2023-06-20 06:42:43 -0700

    print lowercase and uppercase alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,12 @@ int main(void)
 	int a = 97;
 	int b = 65;
 
-	while
-	(a <= 122);
+	while (a <= 122)
 	{
 		putchar(a);
 		a++;
-	} while
-	(b <= 90);
+	}
+	while (b <= 90)
 	{
 		putchar(b);
 		b++;
