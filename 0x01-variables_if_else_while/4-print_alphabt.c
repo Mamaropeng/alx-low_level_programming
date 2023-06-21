86ee2d8eb849c338a4ae8d246a0cf88ee9b3bd65
Mamaropeng <tsholomr12@gmail.com>
2023-06-21 08:43:49 -0700

    print the last digit of the number stored in the variable n

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: 'print lowercase alphabets except q and e'
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 112)
+	{
+	continue; (a != 101 && a != 113);
+	{
+		putchar(a);
+
+	}
+	}
+
+	putchar(a);
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description:
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int a = 48;
+	int q = 36
+
+	while (a != 57)
+	{
+		putchar(a);
+		a++
+	{
+		{
+		
+			putchar(', ');
+			,++;
+
+		}
+	while (q = 36)
+	{
+		putchar(q);
+	}
+	putchar('\n');
+	return (0);
+}
+
