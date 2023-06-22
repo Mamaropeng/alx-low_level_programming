18560c1eed28b3e7640390409425112fab6816ba
Mamaropeng <tsholomr12@gmail.com>
2023-06-22 16:22:01 -0700

    prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square based on number of times
+ * @size: The number of square and number of times
+ * Return: Nothing
+ *
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+	_putchar ('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+		for (b = 0; b < size; b++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+		}
+	}
+}
+
