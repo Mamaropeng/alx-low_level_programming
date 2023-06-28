a9b6d162d30bd8d087be8e02f21cb44d10d529d8
Mamaropeng <tsholomr12@gmail.com>
2023-06-27 18:15:19 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,10 +14,10 @@ void print_array(int *x, int y)
 
 	for (z = 0; z < y; z++)
 	{
-		printf("%d", x[   z]);
+		printf("%d", x[z]);
 		if (z != (y - 1))
 		{
-			printf(",");
+			printf(", ");
 		}
 	}
 	printf("\n");
